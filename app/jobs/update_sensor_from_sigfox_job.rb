5be672c11de83340c1f963fa2ed1995b32ab27c7
Vojta Drbohlav <vojta.d@gmail.com>
2017-11-26 12:17:37 +0100

    fix negative percents

--- app/jobs/update_sensor_from_sigfox_job.rb
+++ app/jobs/update_sensor_from_sigfox_job.rb
@@ -33,15 +33,40 @@ class UpdateSensorFromSigfoxJob < ApplicationJob
       y -= 0x80
       z -= 0x80
 
-      angle = Math.atan(x.to_f / z.to_f)
-      degrees = (angle * 180) / Math::PI
-      pct = (100 * angle / (Math::PI / 2)).round
+      angle_xz = Math.atan2(x.to_f, z.to_f)
+      angle_xy = Math.atan2(x.to_f, y.to_f)
+      angle_yz = Math.atan2(y.to_f, z.to_f)
+
+      degrees_xz = radians_to_degrees(angle_xz)
+      degrees_xy = radians_to_degrees(angle_xy)
+      degrees_yz = radians_to_degrees(angle_yz)
+
+      pct_xz = radians_to_pct(angle_xz)
+      pct_xy = radians_to_pct(angle_xy)
+      pct_yz = radians_to_pct(angle_yz)
 
       puts "Sigfox data: x = #{x.to_s(16)}, y = #{y.to_s(16)}, z = #{z.to_s(16)}"
-      puts "Sigfox data: angle = #{angle.round(2)}, degrees = #{degrees.round(2)}, pct = #{pct}"
+      puts "Sigfox data: angle_xz = #{angle_xz.round(2)}, degrees_xz = #{degrees_xz.round(2)}, pct_xz = #{pct_xz}"
+      puts "Sigfox data: angle_xy = #{angle_xy.round(2)}, degrees_xy = #{degrees_xy.round(2)}, pct_xy = #{pct_xy}"
+      puts "Sigfox data: angle_yz = #{angle_yz.round(2)}, degrees_yz = #{degrees_yz.round(2)}, pct_yz = #{pct_yz}"
+
+      pct = pct_xz
     else
       pct = nil
     end
+
+    pct
+  end
+
+  def radians_to_degrees(radians)
+    (radians * 180) / Math::PI
+  end
+
+  def radians_to_pct(radians)
+    pct = (100 * radians / (Math::PI / 2)).round.abs
+
+    return 0 if pct < 0
+    return 100 if pct > 100
 
     pct
   end
